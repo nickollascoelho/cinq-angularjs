c07c741da416ceaf413b58d1a8a6c46e6351c20a
Nickollas Coelho <nickollas.coelho@gmail.com>
2016-02-17 17:22:32 -0200

    Change person index to start in 1

--- index.js
+++ index.js
@@ -11,9 +11,40 @@ app.use('/rest', proxy('localhost:8090', {
   }
 }));
 
+var generateFakeList = function generateFakeList() {
+  var name = ["Adam", "Abe", "Maria", "Rose", "Mario", "Luigi"];
+  var surname = ["Lincoln", "Franklin", "Jackson", "Miyazaki", "M'bebe"];
+  var people = [];
+
+
+  for (var i = 1; i <= 10; i++) {
+    var namePos = Math.floor((Math.random() * name.length));
+    var surnamePos = Math.floor((Math.random() * surname.length));
+
+    var person = {
+      id: i,
+      name: name[namePos] + " " + surname[surnamePos],
+      disclosableInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini" +
+        "m veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo c"+
+        "onsequat."
+    };
+
+    people.push(person);
+  }
+
+  return people;
+};
+
+app.get('/api/people', function(req, res) {
+   res.send(generateFakeList());
+});
+
 app.use(express.static(__dirname + '/www'));
 
-console.log('Listening to port 3000');
-app.listen(3000);
+var PORT = 3000;
+
+console.log('Listening to port ' + PORT);
+app.listen(PORT);
 
 module.exports = app;
